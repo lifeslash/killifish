0159a62e05595ad02f4b3952b528405dc0b763db
again <lifeslash.mail@gmail.com>
2025-07-10 23:50:26 +0900

    update

---
+++ config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define ENCODER_A_PINS { B5, D2 }
+#define ENCODER_B_PINS { D3, B2 }
+
+#define ENCODER_RESOLUTIONS { 1, 1 }
+
--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -17,12 +17,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │   0   │ . │   │
      * └───────┴───┴───┘
      */
-    [0] = LAYOUT_numpad_5x4(
-        TG(1),   KC_PSLS, KC_PAST, KC_PMNS,
-        KC_P7,   KC_P8,   KC_P9,
-        KC_P4,   KC_P5,   KC_P6,   KC_PPLS,
-        KC_P1,   KC_P2,   KC_P3,
-        KC_P0,   KC_PDOT,          KC_PENT
+    [0] = LAYOUT(
+        KC_A,    KC_B,
+        KC_DEL,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PEQL,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,   KC_PCMM, KC_PDOT
     ),
 
     /*
@@ -38,11 +39,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │Insert │Del│   │
      * └───────┴───┘───┘
      */
-    [1] = LAYOUT_numpad_5x4(
-        _______, _______, _______, _______,
-        KC_HOME, KC_UP,   KC_PGUP,
-        KC_LEFT, XXXXXXX, KC_RGHT, _______,
-        KC_END,  KC_DOWN, KC_PGDN,
-        KC_INS,           KC_DEL,  _______
+    [1] = LAYOUT(
+        KC_C,    KC_D,
+        KC_DEL,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PEQL,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,   KC_PCMM, KC_PDOT
     )
+};
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_UP,   KC_DOWN),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
+    [1] = { ENCODER_CCW_CW(KC_UP,   KC_DOWN),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
 };

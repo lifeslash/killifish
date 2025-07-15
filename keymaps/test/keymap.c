9b68a3f7e4109295cc2c0868e28321520818cda0
again <lifeslash.mail@gmail.com>
2025-07-15 22:19:01 +0900

    update

---
+++ keymaps/test/keymap.c
@@ -0,0 +1,55 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┬───┐
+     * │TG1│ / │ * │ - │
+     * ├───┼───┼───┼───┤
+     * │ 7 │ 8 │ 9 │   │
+     * ├───┼───┼───┤ + │
+     * │ 4 │ 5 │ 6 │   │
+     * ├───┼───┼───┼───┤
+     * │ 1 │ 2 │ 3 │   │
+     * ├───┴───┼───┤Ent│
+     * │   0   │ . │   │
+     * └───────┴───┴───┘
+     */
+    [0] = LAYOUT(
+        KC_A,    KC_B,
+        KC_C,    KC_D,    KC_E,    KC_F,
+        KC_G,    KC_H,    KC_I,    KC_J,
+        KC_K,    KC_L,    KC_M,    KC_N,
+        KC_O,    KC_P,    KC_Q,    KC_R,
+        KC_S,    KC_T,    KC_U
+    ),
+
+    /*
+     * ┌───┬───┬───┬───┐
+     * │TG1│ / │ * │ - │
+     * ┌───┬───┬───┐───┤
+     * │Hom│ ↑ │PgU│   │
+     * ├───┼───┼───┤ + │
+     * │ ← │   │ → │   │
+     * ├───┼───┼───┤───┤
+     * │End│ ↓ │PgD│   │
+     * ├───┴───┼───┤Ent│
+     * │Insert │Del│   │
+     * └───────┴───┘───┘
+     */
+    [1] = LAYOUT(
+        KC_C,    KC_D,
+        KC_DEL,  KC_PSLS, KC_PAST, KC_PMNS,
+        KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        KC_P4,   KC_P5,   KC_P6,   KC_PEQL,
+        KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        KC_P0,   KC_PCMM, KC_PDOT
+    )
+};
+
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_UP,   KC_DOWN),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
+    [1] = { ENCODER_CCW_CW(KC_UP,   KC_DOWN),  ENCODER_CCW_CW(KC_RIGHT, KC_LEFT) },
+};
